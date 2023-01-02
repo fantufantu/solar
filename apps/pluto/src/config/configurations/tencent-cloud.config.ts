4dfd9dd4cd57bb7e1a60669680412a49df687934
murukal <tutu@fantufantu.com>
2023-01-02 20:33:13 +0800

    chore: remove useless code

--- apps/pluto/src/config/configurations/tencent-cloud.config.ts
+++ apps/pluto/src/config/configurations/tencent-cloud.config.ts
@@ -6,11 +6,6 @@ import { ConfigRegisterToken, TencentCloudPropertyToken } from 'assets/tokens';
 export default registerAs<Record<TencentCloudPropertyToken, string>>(
   ConfigRegisterToken.TencentCloud,
   () => {
-    console.log(
-      'process.env.TENCENT_CLOUD_SECRET_ID====',
-      process.env.TENCENT_CLOUD_SECRET_ID,
-    );
-
     return {
       bucket: process.env.BUCKET,
       region: process.env.REGION,
