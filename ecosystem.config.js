b275d38d25104dac96e7c9ef779cfe5126a1f2c9
murukal <tutu@fantufantu.com>
2023-02-11 22:51:08 +0800

    feat: 新增启动脚本

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,5 +10,15 @@ module.exports = {
       script: 'yarn',
       args: 'start:mercury',
     },
+    {
+      name: 'venus',
+      script: 'yarn',
+      args: 'start:venus',
+    },
+    {
+      name: 'halley',
+      script: 'yarn',
+      args: 'start:halley',
+    },
   ],
 };
