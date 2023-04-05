3b2d2216bec67eb4e38c5ee3ad37f1e0b434e8cf
murukal <tutu@fantufantu.com>
2023-04-05 19:50:07 +0800

    fix: class name

--- apps/mercury/src/user/user.module.ts
+++ apps/mercury/src/user/user.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 import { User } from './entities/user.entity';
-import { UserEmail } from './entities/user-verification.entity';
+import { UserVerification } from './entities/user-verification.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserEmail])],
+  imports: [TypeOrmModule.forFeature([User, UserVerification])],
   controllers: [UserController],
   providers: [UserResolver, UserService],
   exports: [UserService],
