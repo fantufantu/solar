5b7cc1de318782fcdd06788d795baae96e0c848e
murukal <tutu@fantufantu.com>
2024-09-16 21:09:43 +0800

    feat: dto schema fix

--- libs/database/src/entities/earth/user.entity.ts
+++ libs/database/src/entities/earth/user.entity.ts
@@ -5,27 +5,20 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
-import { User as _User } from 'assets/dto/user.object';
 
 @ObjectType()
 @Directive('@key(fields: "id")')
 @Entity()
-export class User extends _User {
+export class User {
   @Field(() => Int, {
     description: 'id',
   })
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Field(() => Date, {
-    description: '创建时间',
-  })
   @CreateDateColumn()
   createdAt: Date;
 
-  @Field(() => Date, {
-    description: '更新时间',
-  })
   @UpdateDateColumn()
   updatedAt: Date;
 }
--- libs/database/src/entities/venus/user.entity.ts
+++ libs/database/src/entities/venus/user.entity.ts
@@ -7,13 +7,12 @@ import {
   PrimaryColumn,
   UpdateDateColumn,
 } from 'typeorm';
-import { User as _User } from 'assets/dto/user.object';
 import { Billing } from '@/lib/database/entities/venus/billing.entity';
 
 @ObjectType()
 @Directive('@key(fields: "id")')
 @Entity()
-export class User extends _User {
+export class User {
   @Field(() => Int, {
     description: 'id',
   })
@@ -38,15 +37,9 @@ export class User extends _User {
   })
   defaultBilling?: Billing;
 
-  @Field(() => Date, {
-    description: '创建时间',
-  })
   @CreateDateColumn()
   createdAt: Date;
 
-  @Field(() => Date, {
-    description: '更新时间',
-  })
   @UpdateDateColumn()
   updatedAt: Date;
 }
