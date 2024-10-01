dbcd1432fd4d68223995f824ebee07b54c19ac2e
murukal <tutu@fantufantu.com>
2024-10-01 14:52:07 +0800

    feat: add cors origin

--- apps/halley/src/main.ts
+++ apps/halley/src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap() {
   const application = await NestFactory.create(ApplicationModule);
   await application.listen(ServicePort.Halley);
 
+  application.enableCors({
+    origin: [/aisz.dev/, /fantufantu.com/],
+  });
+
   console.info(
     `halley is running on http://localhost:${ServicePort.Halley}/graphql`,
   );
