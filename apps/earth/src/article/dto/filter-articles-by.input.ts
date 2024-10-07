5c70ff4abb4a74ea9b3300ef6c0ece4b39a191d3
murukal <tutu@fantufantu.com>
2024-10-07 22:31:28 +0800

    fix: use category code string type

--- apps/earth/src/article/dto/filter-articles-by.input.ts
+++ apps/earth/src/article/dto/filter-articles-by.input.ts
@@ -1,7 +1,7 @@
-import { Field, InputType, Int } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class FilterArticlesBy {
-  @Field(() => [Int], { nullable: true, description: '标签code列表' })
-  categoryCodes?: number[];
+  @Field(() => [String], { nullable: true, description: '标签code列表' })
+  categoryCodes?: string[];
 }
