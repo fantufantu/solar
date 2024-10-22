95f87f80b97177720ae572f17413977777e92118
murukal <tutu@fantufantu.com>
2024-10-22 23:29:18 +0800

    feat: 修改用户信息支持修改头像

--- apps/mercury/src/user/dto/update-user-by.input.ts
+++ apps/mercury/src/user/dto/update-user-by.input.ts
@@ -2,4 +2,8 @@ import { InputType, PickType } from '@nestjs/graphql';
 import { User } from '@/libs/database/entities/mercury/user.entity';
 
 @InputType()
-export class UpdateUserBy extends PickType(User, ['nickname'], InputType) {}
+export class UpdateUserBy extends PickType(
+  User,
+  ['nickname', 'avatar'],
+  InputType,
+) {}
