0303c62d2ae3c42e70bc11e40f209b11de86b956
murukal <tutu@fantufantu.com>
2025-06-08 19:07:29 +0800

    feat: 重构 Resume 实体，更新字段名称并移除不必要的属性

--- libs/database/src/entities/mars/resume.entity.ts
+++ libs/database/src/entities/mars/resume.entity.ts
@@ -1,34 +1,19 @@
 import { ObjectType, Field } from '@nestjs/graphql';
-import { Column, DeleteDateColumn, Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { Preset } from 'assets/entities/preset.entity';
 
 @ObjectType()
 @Entity()
 export class Resume extends Preset {
   @Field(() => String, {
-    description: '标题',
+    description: '简历名称',
   })
   @Column()
-  title: string;
+  name: string;
 
   @Field(() => String, {
-    description: '正文',
+    description: '简历正文',
   })
   @Column('longtext')
   content: string;
-
-  @Field(() => String, { nullable: true, description: '封面地址' })
-  @Column({ nullable: true })
-  cover?: string;
-
-  @Column({ comment: '作者id' })
-  createdById: number;
-
-  @Column({
-    comment: '最后更新人id',
-  })
-  updatedById: number;
-
-  @DeleteDateColumn()
-  deletedAt: Date | null;
 }
