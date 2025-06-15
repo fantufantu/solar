c0099da5a113d67543b3977cc48e8dd266ac9cd1
murukal <tutu@fantufantu.com>
2025-06-15 23:19:59 +0800

    feat: add GraphQL and Passport modules to the application

--- apps/mars/src/app.module.ts
+++ apps/mars/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { ResumeModule } from './resume/resume.module';
 import { DatabaseModule } from '@/libs/database';
 import { ApplicationToken } from 'assets/tokens';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
+import { PassportModule } from '@/libs/passport';
 
 @Module({
   imports: [
+    // api
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
+    }),
+    // 鉴权
+    PassportModule,
     // 数据库
     DatabaseModule.forRoot(ApplicationToken.Mars),
     // 简历模块
