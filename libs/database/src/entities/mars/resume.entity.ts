e7d2e28c56d1598f742663403cf629b2f23297b4
murukal <tutu@fantufantu.com>
2025-07-05 23:16:31 +0800

    feat: 新增 CrudUuid 和 PresetUuid 实体以支持通用的 UUID 结构

--- libs/database/src/entities/mars/resume.entity.ts
+++ libs/database/src/entities/mars/resume.entity.ts
@@ -1,23 +1,10 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
-import {
-  BeforeInsert,
-  Column,
-  CreateDateColumn,
-  DeleteDateColumn,
-  Entity,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { ObjectType, Field } from '@nestjs/graphql';
+import { Column, Entity } from 'typeorm';
+import { CrudUuid } from '../any-use/crud-uuid.entity';
 
 @ObjectType()
 @Entity()
-export class Resume {
-  @Field(() => String, {
-    description: 'id',
-  })
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
+export class Resume extends CrudUuid {
   @Field(() => String, {
     description: '简历名称',
   })
@@ -29,61 +16,4 @@ export class Resume {
   })
   @Column('longtext')
   content: string;
-
-  // copied from `Preset` Entity
-  @Field(() => Date, {
-    description: '创建时间',
-  })
-  @CreateDateColumn({
-    name: 'created_at',
-  })
-  createdAt: Date;
-
-  @Field(() => Date, {
-    description: '更新时间',
-  })
-  @UpdateDateColumn({
-    name: 'updated_at',
-  })
-  updatedAt: Date;
-
-  // copied from `Crud` Entity
-  @Field(() => Int, {
-    description: '创建人id',
-  })
-  @Column({ comment: '创建人id', name: 'created_by_id' })
-  createdById: number;
-
-  @Field(() => Int, {
-    description: '最后更新人id',
-  })
-  @Column({
-    comment: '最后更新人id',
-    name: 'updated_by_id',
-  })
-  updatedById: number;
-
-  @DeleteDateColumn({
-    comment: '删除时间',
-    name: 'deleted_at',
-  })
-  deletedAt: Date | null;
-
-  /**
-   * 新建时默认设置更新人为创建人
-   */
-  @BeforeInsert()
-  private _useUpdatedBy() {
-    this.updatedById = this.createdById;
-  }
-
-  /**
-   * 删除当前实例
-   */
-  useDelete(deleteById: number) {
-    return {
-      deletedAt: 'NOW()',
-      updatedById: deleteById,
-    };
-  }
 }
