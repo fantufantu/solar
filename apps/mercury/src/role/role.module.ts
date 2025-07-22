1387eba4b54eee8b4085a8882f33e99e1e3082e1
murukal <tutu@fantufantu.com>
2025-07-22 23:34:20 +0800

    feat(role): add role controller and update module imports
    - Add RoleController to the role module
    - Update module imports to include the new controller

--- apps/mercury/src/role/role.module.ts
+++ apps/mercury/src/role/role.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoleService } from './role.service';
 import { RoleResolver } from './role.resolver';
 import { Role } from '@/libs/database/entities/mercury/role.entity';
+import { RoleController } from './role.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Role])],
+  controllers: [RoleController],
   providers: [RoleResolver, RoleService],
   exports: [RoleService],
 })
