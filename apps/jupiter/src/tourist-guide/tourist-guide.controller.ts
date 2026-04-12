8c8114db6466c8d80e59fd73fa0ee3ccfeb2b38c
murukal <tutu@fantufantu.com>
2026-04-12 22:08:14 +0800

    feat(tourist-guide): 使用 PostSse 装饰器替换 Sse 装饰器并添加装饰器文件

--- apps/jupiter/src/tourist-guide/tourist-guide.controller.ts
+++ apps/jupiter/src/tourist-guide/tourist-guide.controller.ts
@@ -1,13 +1,13 @@
-import { Body, Controller, Sse } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
 import { TouristGuideService } from './tourist-guide.service';
-import { interval, map } from 'rxjs';
 import { PlanInput } from './dto/plan.input';
+import { PostSse } from 'utils/decorators/sse.decorator';
 
 @Controller('tourist-guide')
 export class TouristGuideController {
   constructor(private readonly touristGuideService: TouristGuideService) {}
 
-  @Sse('plan')
+  @PostSse('plan')
   plan(@Body() input: PlanInput) {
     return this.touristGuideService.plan(input);
   }
