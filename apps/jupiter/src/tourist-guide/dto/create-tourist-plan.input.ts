800f8a689254c4e18065e7fa8ba6e09926098791
murukal <tutu@fantufantu.com>
2026-06-07 14:24:12 +0800

    refactor(tourist-plan): update CreateTouristPlanInput fields and remove AttractionInput

--- apps/jupiter/src/tourist-guide/dto/create-tourist-plan.input.ts
+++ apps/jupiter/src/tourist-guide/dto/create-tourist-plan.input.ts
@@ -1,30 +1,12 @@
 import { TouristPlan } from '@/libs/database/entities/jupiter/tourist-plan.entity';
-import { Attraction } from '@/libs/database/entities/jupiter/attraction.entity';
 import { Field, InputType, PickType } from '@nestjs/graphql';
-
-@InputType()
-class AttractionInput extends PickType(
-  Attraction,
-  ['code', 'name', 'cityCode'],
-  InputType,
-) {}
 
 @InputType()
 export class CreateTouristPlanInput extends PickType(
   TouristPlan,
-  ['depatureAt', 'duration'],
+  ['depatureAt', 'duration', 'cityCodes', 'attractionCodes'],
   InputType,
 ) {
-  @Field(() => [String], {
-    description: '出行目的地城市code列表',
-  })
-  cities!: string[];
-
-  @Field(() => [AttractionInput], {
-    description: '出行景区列表',
-  })
-  attractions!: AttractionInput[];
-
   @Field(() => String, {
     description: '出行方案归属方',
   })
